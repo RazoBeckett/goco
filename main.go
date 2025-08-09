197d4237a47cbfa176e574a70e08bfbe788855df
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2025-08-09 19:14:45 +0530

    chore: scaffold with cobra cli

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import "fmt"
+import "github.com/razobeckett/goco/cmd"
 
 func main() {
-	fmt.Println("Hello")
+	cmd.Execute()
 }
