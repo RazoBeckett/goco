fd2b26f5f3f9f6b7f2890ada5591ac2f2acb8504
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2026-03-22 12:59:12 +0530

    feat: introduce orange fang theme introduce a new color scheme for the cli, the orange fang theme, adding a new file internal/cli/theme.go and updating internal/cli/styles.go and main.go to use the new theme

---
+++ internal/cli/theme.go
@@ -0,0 +1,42 @@
+package cli
+
+import (
+	"image/color"
+
+	"charm.land/fang/v2"
+	lipglossv2 "charm.land/lipgloss/v2"
+)
+
+const (
+	electricOrange = "#FF6A00"
+	tangerineShock = "#FF8C1A"
+	sunburstSurge  = "#FF9F40"
+	mangoVolt      = "#FFC266"
+	creamGleam     = "#FFF1E6"
+	deepMocha      = "#392E29"
+	lipstickRed    = "#FD0040"
+)
+
+func FangColorScheme(ld lipglossv2.LightDarkFunc) fang.ColorScheme {
+	return fang.ColorScheme{
+		Base:           ld(lipglossv2.Color(electricOrange), lipglossv2.Color(creamGleam)),
+		Title:          lipglossv2.Color(electricOrange),
+		Description:    ld(lipglossv2.Color(tangerineShock), lipglossv2.Color(creamGleam)),
+		Codeblock:      ld(lipglossv2.Color(deepMocha), lipglossv2.Color(deepMocha)),
+		Program:        ld(lipglossv2.Color(electricOrange), lipglossv2.Color(creamGleam)),
+		DimmedArgument: ld(lipglossv2.Color(sunburstSurge), lipglossv2.Color(mangoVolt)),
+		Comment:        ld(lipglossv2.Color(tangerineShock), lipglossv2.Color(mangoVolt)),
+		Flag:           ld(lipglossv2.Color(tangerineShock), lipglossv2.Color(mangoVolt)),
+		FlagDefault:    ld(lipglossv2.Color(sunburstSurge), lipglossv2.Color(creamGleam)),
+		Command:        ld(lipglossv2.Color(electricOrange), lipglossv2.Color(tangerineShock)),
+		QuotedString:   ld(lipglossv2.Color(tangerineShock), lipglossv2.Color(creamGleam)),
+		Argument:       ld(lipglossv2.Color(electricOrange), lipglossv2.Color(creamGleam)),
+		Help:           lipglossv2.Color(sunburstSurge),
+		Dash:           lipglossv2.Color(tangerineShock),
+		ErrorHeader: [2]color.Color{
+			lipglossv2.Color(creamGleam),
+			lipglossv2.Color(lipstickRed),
+		},
+		ErrorDetails: lipglossv2.Color(lipstickRed),
+	}
+}
